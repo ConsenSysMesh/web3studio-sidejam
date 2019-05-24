0dafc1ae36504100974ddad2446ecff8374b53b4
Brian Chamberlain <brian.chamberlain@consensys.net>
2019-05-23 21:57:52 -0400

    Added documentation

--- packages/message-bus/contracts/MessageReceiver.sol
+++ packages/message-bus/contracts/MessageReceiver.sol
@@ -2,33 +2,58 @@ pragma solidity ^0.5.0;
 
 contract MessageReceiver {
 
+  // Each receiver manages messages for only one topic
   string public topicName;
+  // Message key to message mapping for look-up
   mapping(bytes32 => Message) public messages;
+  // Message key stored in an array for ordering/indexing
   bytes32[] public messageList;
 
-  event MessageReceived(address msgFrom, bytes32 msgKey);
+  // Event to notify listening/interested parties
+  event MessageReceived(string topicName, address msgFrom, bytes32 msgKey);
 
   struct Message {
     address sender;
     string message;
   }
 
-  constructor (string memory _topicName) public {
-    topicName = _topicName;
+  /**
+  * @notice Create a new MessageReceiver for a specific topic
+  *
+  * @param topic string The topic name for the messages
+  */
+  constructor (string memory topic) public {
+    topicName = topic;
   }
 
+  /**
+  * @notice Send a new message to the "bus". Emits a MessageReceived event.
+  *
+  * @param key bytes32 The key for your message, should be unique
+  * @param newMessage string The message contents
+  */
   function sendMessage(bytes32 key, string memory newMessage) public {
     messageList.push(key);
     messages[key].sender = msg.sender;
     messages[key].message = newMessage;
 
-    emit MessageReceived(msg.sender, key); 
+    emit MessageReceived(topicName, msg.sender, key); 
   }
 
+  /**
+  * @notice Gets the count of the messages sent to this topic
+  * 
+  * @return uint256 Count of messages
+  */
   function getMessageCount() public view returns(uint256) {
     return messageList.length;
   }
 
+  /**
+  * @notice Gets a message given a key
+  *
+  * @return address, string Returns the sender address and message contents 
+  */
   function getMessage(bytes32 key) public view returns (address, string memory) {
     return (messages[key].sender, messages[key].message);
   }
